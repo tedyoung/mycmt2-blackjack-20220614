88f1db5b1cc215c64f3fa6be6b715ce66e5fd1da
Ted M. Young <tedyoung@gmail.com>
2022-06-14 12:21:55 -0700

    LAB #2: Extracted (from display()) and wrote characterization test for cardsAsString

--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -8,11 +8,21 @@ import static org.assertj.core.api.Assertions.*;
 
 public class HandDisplayTest {
 
-  @Test
-  public void displayFaceUpCard() throws Exception {
-    Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
+    @Test
+    public void displayFaceUpCard() throws Exception {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
 
-    assertThat(ConsoleHand.displayFaceUpCard(hand))
-        .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
-  }
+        assertThat(ConsoleHand.displayFaceUpCard(hand))
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
+
+    @Test
+    public void cardsAsStringTransformsWholeHand() throws Exception {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.FIVE),
+                                     new Card(Suit.SPADES, Rank.JACK)));
+
+        assertThat(hand.cardsAsString())
+                .isEqualTo("[31m┌─────────┐[1B[11D│5        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        5│[1B[11D└─────────┘[6A[1C[30m┌─────────┐[1B[11D│J        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        J│[1B[11D└─────────┘");
+    }
+
 }
