f36756e82536646c8807a2d13b7040aa3920ece3
Ted M. Young <tedyoung@gmail.com>
2022-06-15 12:24:48 -0700

    LAB #4: Extracted Enum (via extract delegate) to form GameOutcome and adjusted tests.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -28,7 +28,7 @@ public class ConsoleGame {
 
         displayFinalGameState(game);
 
-        System.out.println(game.determineOutcome());
+        System.out.println(game.determineOutcome().display());
 
         resetScreen();
     }
--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -14,7 +14,7 @@ class GameOutcomeTest {
         game.playerHits();
 
         assertThat(game.determineOutcome())
-                .isEqualTo("You Busted, so you lose.  💸");
+                .isEqualByComparingTo(GameOutcome.PLAYER_BUSTED);
     }
 
     @Test
@@ -26,7 +26,7 @@ class GameOutcomeTest {
         game.dealerTurn();
 
         assertThat(game.determineOutcome())
-                .isEqualTo("You beat the Dealer! 💵");
+                .isEqualByComparingTo(GameOutcome.PLAYER_BEATS_DEALER);
     }
 
 }
