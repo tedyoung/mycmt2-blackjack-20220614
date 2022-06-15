043800a8f41e1f0f8a3098c99b67fdba0a7737c7
Ted M. Young <tedyoung@gmail.com>
2022-06-15 11:18:06 -0700

    LAB #3: Tidy-up'd the ConsoleGame class, making method private and non-static as much as possible.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -16,75 +16,6 @@ public class ConsoleGame {
         this.game = game;
     }
 
-    public static void resetScreen() {
-        System.out.println(ansi().reset());
-    }
-
-    public static void waitForEnterFromUser() {
-        System.out.println(ansi()
-                                   .cursor(3, 1)
-                                   .fgBrightBlack().a("Hit [ENTER] to start..."));
-
-        System.console().readLine();
-    }
-
-    public static void displayWelcomeScreen() {
-        AnsiConsole.systemInstall();
-        System.out.println(ansi()
-                                   .bgBright(Ansi.Color.WHITE)
-                                   .eraseScreen()
-                                   .cursor(1, 1)
-                                   .fgGreen().a("Welcome to")
-                                   .fgRed().a(" JitterTed's")
-                                   .fgBlack().a(" BlackJack game"));
-    }
-
-    public static String inputFromPlayer() {
-        System.out.println("[H]it or [S]tand?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
-    public static void displayBackOfCard() {
-        System.out.print(
-                ansi()
-                        .cursorUp(7)
-                        .cursorRight(12)
-                        .a("┌─────────┐").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("└─────────┘"));
-    }
-
-    public static void displayGameState(Game game) {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.displayFaceUpCard(game.dealerHand()));
-
-        // second card is the hole card, which is hidden, or "face down"
-        displayBackOfCard();
-
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
-        System.out.println(" (" + game.playerHand().value() + ")");
-    }
-
-    public static void displayFinalGameState(Game game) {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.cardsAsString(game.dealerHand()));
-        System.out.println(" (" + game.dealerHand().value() + ")");
-
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
-        System.out.println(" (" + game.playerHand().value() + ")");
-    }
-
     public void start() {
         displayWelcomeScreen();
         waitForEnterFromUser();
@@ -102,20 +33,76 @@ public class ConsoleGame {
         resetScreen();
     }
 
-    public void playerPlays() {
+    private void playerPlays() {
         while (!game.isPlayerDone()) {
-            displayGameState(game);
+            displayGameState();
             String command = inputFromPlayer();
             handle(command);
         }
     }
 
-    public void handle(String command) {
+    private void handle(String command) {
         if (command.toLowerCase().startsWith("h")) {
             game.playerHits();
         } else if (command.toLowerCase().startsWith("s")) {
             game.playerStands();
         }
+    }
+
+    private void resetScreen() {
+        System.out.println(ansi().reset());
+    }
+
+    private void waitForEnterFromUser() {
+        System.out.println(ansi()
+                                   .cursor(3, 1)
+                                   .fgBrightBlack().a("Hit [ENTER] to start..."));
+
+        System.console().readLine();
+    }
+
+    private void displayWelcomeScreen() {
+        AnsiConsole.systemInstall();
+        System.out.println(ansi()
+                                   .bgBright(Ansi.Color.WHITE)
+                                   .eraseScreen()
+                                   .cursor(1, 1)
+                                   .fgGreen().a("Welcome to")
+                                   .fgRed().a(" JitterTed's")
+                                   .fgBlack().a(" BlackJack game"));
+    }
+
+
+    private String inputFromPlayer() {
+        System.out.println("[H]it or [S]tand?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private void displayGameState() {
+        System.out.print(ansi().eraseScreen().cursor(1, 1));
+        System.out.println("Dealer has: ");
+        System.out.println(ConsoleHand.displayFaceUpCard(game.dealerHand()));
+
+        // second card is the hole card, which is hidden, or "face down"
+        ConsoleCard.displayBackOfCard();
+
+        System.out.println();
+        System.out.println("Player has: ");
+        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
+        System.out.println(" (" + game.playerHand().value() + ")");
+    }
+
+    private void displayFinalGameState(Game game) {
+        System.out.print(ansi().eraseScreen().cursor(1, 1));
+        System.out.println("Dealer has: ");
+        System.out.println(ConsoleHand.cardsAsString(game.dealerHand()));
+        System.out.println(" (" + game.dealerHand().value() + ")");
+
+        System.out.println();
+        System.out.println("Player has: ");
+        System.out.println(ConsoleHand.cardsAsString(game.playerHand()));
+        System.out.println(" (" + game.playerHand().value() + ")");
     }
 
 }
