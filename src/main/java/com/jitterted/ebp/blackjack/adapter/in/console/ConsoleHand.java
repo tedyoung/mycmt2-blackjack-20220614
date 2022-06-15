dd4a679564fdd708f69b058bcffb5672604a70cd
Ted M. Young <tedyoung@gmail.com>
2022-06-15 10:23:19 -0700

    LAB #3: Re-package classes (including tests) according to Hexagonal Architecture layering

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleHand.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleHand.java
@@ -1,4 +1,6 @@
-package com.jitterted.ebp.blackjack;
+package com.jitterted.ebp.blackjack.adapter.in.console;
+
+import com.jitterted.ebp.blackjack.domain.Hand;
 
 import java.util.stream.Collectors;
 
@@ -7,7 +9,7 @@ import static org.fusesource.jansi.Ansi.ansi;
 public class ConsoleHand {
 
     // CONVERT DOMAIN object (Hand) to CONSOLE display String
-    static String displayFaceUpCard(Hand hand) {
+    public static String displayFaceUpCard(Hand hand) {
         return ConsoleCard.display(hand.faceUpCard());
     }
 
